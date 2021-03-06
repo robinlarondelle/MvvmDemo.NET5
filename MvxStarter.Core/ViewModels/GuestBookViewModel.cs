bdbaa795e9b48eff20517c659944e5d8e04accc0
Robin La Rondelle <robinlarondelle@hotmail.com>
2021-03-06 23:43:03 +0100

    Add project files.

---
+++ MvxStarter.Core/ViewModels/GuestBookViewModel.cs
@@ -0,0 +1,70 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using MvxStarter.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MvxStarter.Core.ViewModels
+{
+	class GuestBookViewModel : MvxViewModel
+	{
+		public GuestBookViewModel()
+		{
+			AddGuestCommand = new MvxCommand(AddGuest);
+		}
+
+		public IMvxCommand AddGuestCommand { get; set; }
+
+		public void AddGuest()
+		{
+			PersonModel p = new PersonModel
+			{
+				FirstName = FirstName,
+				LastName = LastName
+			};
+
+			//Clear own properties after use
+			FirstName = string.Empty;
+			LastName = string.Empty;
+
+			People.Add(p);
+		}
+
+		public bool CanAddGuest => FirstName?.Length > 0 && LastName?.Length > 0;
+
+		private ObservableCollection<PersonModel> _people = new ObservableCollection<PersonModel>();
+
+		public ObservableCollection<PersonModel> People {
+			get { return _people; }
+			set { SetProperty(ref _people, value); }
+		}
+
+		private string _firstName;
+
+		public string FirstName {
+			get { return _firstName; }
+			set {
+				SetProperty(ref _firstName, value);
+				RaisePropertyChanged(() => FullName);
+				RaisePropertyChanged(() => CanAddGuest);
+			}
+		}
+
+		private string _lastName;
+
+		public string LastName {
+			get { return _lastName; }
+			set {
+				SetProperty(ref _lastName, value);
+				RaisePropertyChanged(() => FullName);
+				RaisePropertyChanged(() => CanAddGuest);
+
+			}
+		}
+
+		//Read only property
+		public string FullName => $"{FirstName} {LastName}";
+	}
+}
---
+++ MvxStarterWPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MvvmCross.Core;
+using MvvmCross.Platforms.Wpf.Core;
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace MvxStarterWPF
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : MvxApplication
+	{
+		protected override void RegisterSetup()
+		{
+			this.RegisterSetupType<MvxWpfSetup<MvxStarter.Core.App>>();
+		}
+	}
+}
